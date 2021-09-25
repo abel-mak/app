b9e25ce0122a831ab8e4557dfba340d44f35339b
Abdelali El makkioui <abel-mak@e1r6p9.1337.ma>
2021-09-25 18:33:00 +0100

    fix errors related to the last commit

--- server/app.js
+++ server/app.js
@@ -5,18 +5,18 @@ const cookieParser = require('cookie-parser');
 const app          = express();
 const path         = require('path');
 
-app.set('views', path.join(__dirname, "./views"));
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 
-//app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cookieParser());
 
 app.use(router);
 
-app.use(express.static(path.join(__dirname, "./static")));
+app.use(express.static(path.join(__dirname, './static')));
 
 app.listen(3000, () => console.log('listen on 3000'));
 
